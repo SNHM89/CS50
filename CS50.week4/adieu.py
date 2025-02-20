46dfee71b09a9fa276b617a228e22c31cef4320d
SNHM89 <nourazmoghaddam@gmail.com>
2025-02-20 20:11:55 +0330

    week4

---
+++ CS50.week4/adieu.py
@@ -0,0 +1,12 @@
+import inflect
+p = inflect.engine()
+name_list = []
+while True:
+    try:
+        name = input("Name: ")
+        name_list.append(name)
+    except EOFError:
+        print()
+        break
+output = p.join(name_list)
+print(f"Adieu, adieu, to {output}")
